2053c7150784972a1c1220a3b86d505dba5b0c75
JohanRuiz05 <68175218+JohanRuiz05@users.noreply.github.com>
2025-10-26 14:19:58 -0500

    Update femto.v

--- src/femto.v
+++ src/femto.v
@@ -218,5 +218,8 @@ module tt_um_femto (
 	// List all unused inputs to prevent warnings
 	
 	wire _unused = &{ena, uio_oe[7:0], uio_in[7:0], uio_out[7:0], ui_in[7:4], ui_in[2:1], 1'b0};
+
+	assign uio_out = 8'h00;
+	assign uio_oe = 8'h00;
 	
 endmodule
