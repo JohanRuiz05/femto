56fd5dab27f03b0a12309ed05b2c04cb8edd91f1
JohanRuiz05 <68175218+JohanRuiz05@users.noreply.github.com>
2025-10-29 08:21:42 -0500

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -5,38 +5,37 @@
    that can be driven / tested by the cocotb test.py.
 */
 module tb ();
-	
-	// Dump the signals to a VCD file. You can view it with gtkwave or surfer.
-	initial begin
-    	$dumpfile("tb.vcd");
-    	$dumpvars(0, tb);
-   		#1;
-  	end
-   
-   // Wire up the inputs and outputs:
-	reg clk;
-	reg rst_n;
-	reg ena;
-	reg [7:0] ui_in;
-	reg [7:0] uio_in;
-	wire [7:0] uo_out;
-	wire [7:0] uio_out; 
-	wire [7:0] uio_oe;
-	
-	`ifdef GL_TEST
-	wire VPWR = 1'b1;
-  	wire VGND = 1'b0;
-	`endif
-	
+
+  // Dump the signals to a VCD file. You can view it with gtkwave or surfer.
+  initial begin
+    $dumpfile("tb.vcd");
+    $dumpvars(0, tb);
+    #1;
+  end
+
+  // Wire up the inputs and outputs:
+  reg clk;
+  reg rst_n;
+  reg ena;
+  reg [7:0] ui_in;
+  reg [7:0] uio_in;
+  wire [7:0] uo_out;
+  wire [7:0] uio_out;
+  wire [7:0] uio_oe;
+`ifdef GL_TEST
+  wire VPWR = 1'b1;
+  wire VGND = 1'b0;
+`endif
 
   // Replace tt_um_example with your module name:
   tt_um_femto user_project (
-	  // Include power ports for the Gate Level test:
-	  `ifdef GL_TEST
+
+      // Include power ports for the Gate Level test:
+`ifdef GL_TEST
       .VPWR(VPWR),
       .VGND(VGND),
-	  `endif
-	  
+`endif
+
       .ui_in  (ui_in),    // Dedicated inputs
       .uo_out (uo_out),   // Dedicated outputs
       .uio_in (uio_in),   // IOs: Input path
